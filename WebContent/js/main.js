beffcfdded7b57dce58c7e4ac46dd67965137545
hishi <ev422119.s.okayama@gmail.com>
2017-09-04 22:17:34 +0900

    2017/9/4commit

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -140,37 +140,48 @@ $(document).ready(function() {
     $("#submit").click(function(){
 
       // 入力値格納用配列
-      var checkval = [];
-//      var email = [];
-//      var passwd = [];
-//      var insdate = [];
-//      var upddate = [];
+      var array = [2][4];
+      array[0][0] = "aaaaa@yahoo";
+      array[0][1] = "pass";
+      array[0][2] = "1111";
+      array[0][3] = "22222";
+      array[1][0] = "bbb@co.jp";
+      array[1][1] = "passwd";
+      array[1][2] = "date";
+      array[1][3] = "9999";
 
-      var email = "";
-      var passwd = "";
-      var insdate = "";
-      var upddate = "";
+
+      var checkval = [];
+      var email = [];
+      var passwd = [];
+      var insdate = [];
+      var upddate = [];
+
+//      var email = "";
+//      var passwd = "";
+//      var insdate = "";
+//      var upddate = "";
 
 
 
       // 選択済み行のみ取得
       $('input[name="check"]:checked').each(function() {
 
-        // value値を配列に格納
-        checkval.push($(this).val());
+    	  // value値を配列に格納
+    	  checkval.push($(this).val());
 
-        // 選択済み行の入力値を配列に格納      ※選択checkbox→td要素→input type=text
+    	  // 選択済み行の入力値を配列に格納      ※選択checkbox→td要素→input type=text
 
-        // !!!!!!2017/8/19 リストで送る方法がよくわからないので、とりあえず一行のみ送る　あとでやる！
-        //email.push($(this).parent().parent().find('td').eq(2).find("input").val());
-//        passwd.push($(this).parent().parent().find('td').eq(3).find("input").val());
-//        insdate.push($(this).parent().parent().find('td').eq(4).find("input").val());
-//        upddate.push($(this).parent().parent().find('td').eq(5).find("input").val());
+    	  // !!!!!!2017/8/19 リストで送る方法がよくわからないので、とりあえず一行のみ送る　あとでやる！
+    	  email.push($(this).parent().parent().find('td').eq(2).find("input").val());
+    	  passwd.push($(this).parent().parent().find('td').eq(3).find("input").val());
+    	  insdate.push($(this).parent().parent().find('td').eq(4).find("input").val());
+    	  upddate.push($(this).parent().parent().find('td').eq(5).find("input").val());
 
-        email = ($(this).parent().parent().find('td').eq(2).find("input").val());
-        passwd = ($(this).parent().parent().find('td').eq(3).find("input").val());
-        insdate = ($(this).parent().parent().find('td').eq(4).find("input").val());
-        upddate = ($(this).parent().parent().find('td').eq(5).find("input").val());
+//        email = ($(this).parent().parent().find('td').eq(2).find("input").val());
+//        passwd = ($(this).parent().parent().find('td').eq(3).find("input").val());
+//        insdate = ($(this).parent().parent().find('td').eq(4).find("input").val());
+//        upddate = ($(this).parent().parent().find('td').eq(5).find("input").val());
 
 
 
@@ -181,7 +192,8 @@ $(document).ready(function() {
               url: 'sample',
               type: 'POST',
               dataType: 'html', // レスポンスの データ形式なので記載不要？
-              data : {'email' : email, 'passwd' : passwd, 'insdate' : insdate, 'upddate' : upddate }
+        	  data : array
+              //data : {'email' : email, 'passwd' : passwd, 'insdate' : insdate, 'upddate' : upddate }
       }).done(function(data) {
                         alert("success");
       }).fail(function(XMLHttpRequest, textStatus, errorThrown) {
